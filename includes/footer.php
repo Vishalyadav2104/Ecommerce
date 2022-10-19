446532933d7afa0db1e8e2402d7f4770845cc079
Vishalyadav2104 <vy19992104@gmail.com>
2022-10-20 05:10:04 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+	<div class="container">
+		<center>Copyright <span class="glyphicon glyphicon-copyright-mark"></span> Vishal Yadav 
+                    | All Rights Reserved | Contact Us: +91 7500146536</center>
+	</div>
+</footer>
